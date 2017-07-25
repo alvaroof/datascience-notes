a71fe71a3f88320fe603a1a36f09d1a2b0977b26
Alvaro Ortiz Fernandez <alvaro.ortizfernandez@bayer.com>
2017-07-25 15:09:30 +0200

    25.07.2017

--- summary.R
+++ summary.R
@@ -147,7 +147,7 @@ qnorm() #quantile
 
 set.seed(144) #in simulation is important to set seeds to we can reproduce results!
 
-sample(1:10, replace = TRUE)
+sample(1:10, replace = TRUE) #create random vector from a sample
 
 ##PROFILING
 system.time(str(mtcars)) #gives the elapsed execution time for the input expression
@@ -170,12 +170,15 @@ iris %>% mutate(L = Sepal.Length - Petal.Length, W = Sepal.Width - Petal.Width) 
     group_by(Species) %>% summarize(LDev = sum(L - mean(L)), WDev = sum(W - mean(W)))
 
 
+
 #INDEXES
 match() #returns happeing index of an element in within an array
 
 
-
+##########################################################
 ###GETTING AND CLEANING DATA
+#############################################################
+
 ##DOWNLOAD FILES and REad FILES
 download.file() #downloads a file from the internet. Useful for downloading csv 
 #files, excel files, and so on.
@@ -212,6 +215,193 @@ cat(myjson)
 #data.table datatables (really fast, should give it a try when working with large files)
 library(data.table) #works as data.frame, but faster. creates objects of 
 #class table
+
+table(iris$Sepal.Length, useNA = "ifany") #count number of occurences
+xt <- xtabs(iris$Sepal.Length ~ iris$Sepal.Width + iris$Petal.Width ) #make crosstabs
+ftable(xt) #better visualization
+
+
+##check for missing values
+sum(is.na(iris$Petal.Width))
+any(is.na(iris$Petal.Width))
+all(iris$Sepal.Length>0)
+colSums(is.na(iris))
+all(colSums(is.na(iris)))
+table(iris$Species %in% c("versicolor", "virginica"))
+
+
+##MYSQL
+library(RMySQL)
+library(sqldf)
+#see databases in server
+ucscDb <- dbConnect(MySQL(),user="genome",host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb, "show databases;"); 
+dbDisconnect(ucscDb)
+
+#connect to specific database
+hg19 <- dbConnect(MySQL(),user="genome", db="hg19", host="genome-mysql.cse.ucsc.edu")
+allTables <- dbLisTables(hg19) 
+length(allTables)
+
+#see the fields of a particular table
+dbListFields(hg19, "table")
+
+#convert table to a dataframe
+affyData <- dbReadTable(hg19, "table")
+head(affyData)
+
+#select a specfici subset
+query <- dbSendquery(hg19, "select * from affyU133Plus2 where blablabla")
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+affyMisSmall <- fetch(query, n=10)
+dbClearResult(query)
+
+##ACCESING WEBS
+library(httr)
+library(XML)
+url <- "https://www.bioconductor.org/install/"
+html2 <- GET(url)
+content2 <- content(html2, as="text")
+parsedHtml <- htmlParse(content2, asText=TRUE)
+xpathSApply(parsedHtml, "///title", xmlValue)
+
+#for authentications
+html2 <- GET(url, authenticate("user","passwd"))
+
+
+##READING IMAGES
+install.packages("png")
+install.packages("jpeg")
+install.packages("readbitmap")
+
+library("jpeg")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg", destfile = "image.jpeg")
+img <- readJPEG("image.jpeg", native = TRUE) ##native = TRUE reads the jpeg in 
+#nativeRaster format
+
+
+##READING GIS
+install.packages("rdgal")
+install.packages("rgeos")
+install.packages("raster")
+
+##READING MUSIC DATA
+install.packages("tuneR")
+install.packages("seewave")
+
+
+
+##GITHUB APIs
+library(httr)
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+# 2. To make your own application, register at at
+#    https://github.com/settings/applications. Use any URL for the homepage URL
+#    (http://github.com is fine) and  http://localhost:1410 as the callback url
+#
+#    Replace your key and secret below.
+
+myapp <- oauth_app("github",
+                   key = "cbdbff315748875caa0f",
+                   secret = "d622f9e28f3262b3d0e633be2a10c1c2093db573")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+content(req)
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
+stop_for_status(req)
+content(req)
+
+##COMPARISONS
+identical(a1r, a1s)
+
+##read files with width fixed
+x <- read.fwf(
+  file=url("http://www.cpc.ncep.noaa.gov/data/indices/wksst8110.for"),
+  skip=4,
+  widths=c(12, 7, 4, 9, 4, 9, 4, 9, 4))
+
+
+#Know the size of a file
+object.size(iris)
+
+##CREAITNG NEW VAR
+#creating sequences
+s1 <- seq(1,10,by=2)
+s2 <- seq(1,100,length = 4)
+
+##creating binary variables
+iris$large <- ifelse(iris$Sepal.Length>mean(iris$Sepal.Length), TRUE,FALSE)
+table(iris$large)
+
+#creating categorical variables
+cutpoints <- quantile(iris$Sepal.Length, seq(0,1,length=5), na.rm = TRUE)
+iris$groups <- cut(iris$Sepal.Length, cutpoints)
+table(iris$groups)
+
+
+#RESHAPING DATASETS
+
+#melting dataframes: simplify datasets setting which var are ids(keys) and which are measures
+library(reshape2)
+mtcars$carname <- rownames(mtcars)
+carMelt <- melt(mtcars, id=c("carname", "gear", "cyl"), measure.vars = c("mpg", "hp")) #melt reshape the dataframe
+head(carMelt, n=3)
+
+#casting dataframes: reshape dataframes to summarize one var in functions of others. By default summarize by length
+cylData <- dcast(carMelt, cyl ~ variable, mean)
+
+#averaging values
+tapply(InsectSprays$count, InsectSprays$spray, sum)
+
+#split into a list
+l <- split(iris$Sepal.Length, iris$Species)
+lapply(l, sum)
+unlist(l)
+
+#intersect of two group
+intersect(names(iris), c("Species"))
+
+#merging data: like doing joins in SQL
+library(dplyr)
+colClasses = c("character", "character", "character", "character",
+               "character", "character", "character", "character", "character", "character")
+
+gdp <- read.csv(url("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"), 
+                skip = 4, colClasses = colClasses)
+edu <- read.csv(url("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"))
+gdp <- rename(gdp, Id = X)
+edu <- rename(edu, Id = CountryCode)
+
+df <- merge(gdp, edu, by = "Id") #slower than join, but more featured
+df2 <- merge(gdp, edu, by = "Id") #better for multiple dfs
+
+df <- rename(df, gdp = X.4)
+df <- mutate(df, gdp = as.numeric(gsub(",","",gdp)))
+df <- select(df, gdp, Income.Group)
+gdp_groups <- group_by(df, Income.Group)
+summarize(gdp_groups, mean = mean(gdp, na.rm = TRUE))
+
+df2$rank <- as.numeric(df2$X.1)
+df2 <- select(df2, rank, Income.Group)
+rank_groups <- group_by(df2, Income.Group)
+summarize(rank_groups, mean = mean(rank, na.rm = TRUE))
+
+cutpoints <- quantile(df2$rank, seq(0,1,length=5), na.rm = TRUE)
+df2$quantile <- cut(df2$rank, cutpoints)
+
+
+#####################################################################
+##EXPLORATORY DATA ANALYSIS
+#####################################################################
 
 ##EXPLORATORY DATA ANALYSIS
 ##GRAPHS
@@ -280,70 +470,15 @@ qplot(iris$Sepal.Length, iris$Sepal.Width)
 pdf(file = "myplot.pdf") 
 par(mfrow = c(1,3), mar = c(4,4,2,1), oma = c(0,0,2,0))
 with(airquality, {
-plot(Wind, Ozone, main = "Ozone ~ Wind")
-plot(Solar.R, Ozone, main = "Ozone ~ Solar.R")
-plot(Temp, Ozone, main = "Ozone ~ Temp")
-mtext("Ozone and Weather in NY", outer = TRUE)
+    plot(Wind, Ozone, main = "Ozone ~ Wind")
+    plot(Solar.R, Ozone, main = "Ozone ~ Solar.R")
+    plot(Temp, Ozone, main = "Ozone ~ Temp")
+    mtext("Ozone and Weather in NY", outer = TRUE)
 })
 dev.off()
 
 
 dev.cur() #currently open graphic devices 
-
-
-##MYSQL
-library(RMySQL)
-library(sqldf)
-#see databases in server
-ucscDb <- dbConnect(MySQL(),user="genome",host="genome-mysql.cse.ucsc.edu")
-result <- dbGetQuery(ucscDb, "show databases;"); 
-dbDisconnect(ucscDb)
-
-#connect to specific database
-hg19 <- dbConnect(MySQL(),user="genome", db="hg19", host="genome-mysql.cse.ucsc.edu")
-allTables <- dbLisTables(hg19) 
-length(allTables)
-
-#see the fields of a particular table
-dbListFields(hg19, "table")
-
-#convert table to a dataframe
-affyData <- dbReadTable(hg19, "table")
-head(affyData)
-
-#select a specfici subset
-query <- dbSendquery(hg19, "select * from affyU133Plus2 where blablabla")
-affyMis <- fetch(query); quantile(affyMis$misMatches)
-affyMisSmall <- fetch(query, n=10)
-dbClearResult(query)
-
-##ACCESING WEBS
-library(httr)
-library(XML)
-url <- "https://www.bioconductor.org/install/"
-html2 <- GET(url)
-content2 <- content(html2, as="text")
-parsedHtml <- htmlParse(content2, asText=TRUE)
-xpathSApply(parsedHtml, "///title", xmlValue)
-
-#for authentications
-html2 <- GET(url, authenticate("user","passwd"))
-
-
-##READING IMAGES
-install.packages("png")
-install.packages("jpg")
-install.packages("readbitmap")
-
-##READING GIS
-install.packages("rdgal")
-install.packages("rgeos")
-install.packages("raster")
-
-##READING MUSIC DATA
-install.packages("tuneR")
-install.packages("seewave")
-
 
 ##visualization with lattice
 #panel function serves to personalize what is shown
@@ -351,44 +486,6 @@ xyplot(airquality$Ozone ~ airquality$Wind | airquality$Month, panel = function(x
     panel.xyplot(x,y,...)
     panel.lmline(x,y, col = 2)
 })
-
-##GITHUB APIs
-library(httr)
-# 1. Find OAuth settings for github:
-#    http://developer.github.com/v3/oauth/
-oauth_endpoints("github")
-# 2. To make your own application, register at at
-#    https://github.com/settings/applications. Use any URL for the homepage URL
-#    (http://github.com is fine) and  http://localhost:1410 as the callback url
-#
-#    Replace your key and secret below.
-
-myapp <- oauth_app("github",
-                   key = "cbdbff315748875caa0f",
-                   secret = "d622f9e28f3262b3d0e633be2a10c1c2093db573")
-
-# 3. Get OAuth credentials
-github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
-
-# 4. Use API
-gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
-stop_for_status(req)
-content(req)
-
-# OR:
-req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
-stop_for_status(req)
-content(req)
-
-##COMPARISONS
-identical(a1r, a1s)
-
-##read files with width fixed
-x <- read.fwf(
-  file=url("http://www.cpc.ncep.noaa.gov/data/indices/wksst8110.for"),
-  skip=4,
-  widths=c(12, 7, 4, 9, 4, 9, 4, 9, 4))
 
 ##VISUALIZACION CON GGPLOT2
 #qplot with two variables is similar to plot
@@ -411,11 +508,108 @@ qplot(iris$Sepal.Length, iris$Sepal.Width,
 geom_smooth(method = "lm")
 
 ##how to use generic ggplot
-g <- ggplot(iris, aes(Sepal.Length, Sepal.Width))
+
 g + geom_point(aes(color = Species), size = 5) +
     labs(title = "ggplot2 example") +
     labs(x = expression("log " * PM[2.5]), y = "Width") +
     geom_smooth(method = "lm") + 
     facet_grid(. ~ Species) +
-    theme_bw()
+    theme_bw() +
+    coord_cartesian(ylim = c(2.5,4.0))
+
+
+cutpoints <- quantile(iris$Petal.Length, seq(0,1,length=5), na.rm = TRUE)
+iris$Petal.factor <- cut(iris$Petal.Length, cutpoints)
+g <- ggplot(iris, aes(Sepal.Length, Sepal.Width))
+g + geom_point(aes(color = Species)) +
+    facet_grid(Petal.factor ~ Species) +
+    theme_bw() 
+
+
+##HIERARCHYCAL CLUSTERING
+#let's generate and annotate random data points 
+set.seed(1234)
+par(mar = c(0,0,0,0))
+x <- rnorm(12, mean = rep(1:3, each = 4), sd = 0.2)
+y <- rnorm(12, mean = rep(c(1,2,1), each = 4), sd = 0.2)
+plot(x,y, col="blue", pch=19,cex=2)
+text(x+0.05,y+0.05, labels = as.character(1:12))
+
+dataFrame <- data.frame(x=x, y=y)
+distxy <- dist(dataFrame) #by default is the euclidean distance metric
+
+#hierarchical clustering
+#good thing about hclust is that it doesn't tell you how many cluster there are
+#this is something that you define 'cutting' the tree
+#when calculating distance between cluster there are two common methods
+#average linkage: distance between center of gravity
+#complete linkage: distance is the distance between farthest points.
+#advice: try to scale variables before applying
+hClustering <- hclust(distxy)
+plot(hClustering)
+
+#heatmap function for visualizing matrix data
+#what heatmap basically does is to run a hierarchical clustering
+#both in the rows and in the columns of the matrix
+set.seed(144)
+dataMatrix <- as.matrix(dataFrame)[sample(1:12),]
+heatmap(dataMatrix)
+
+
+##K-MEANS CLUSTERING
+#contrary to hierarchical clustering, k menas needs to know the number
+#of clusters in advance.
+#let's generate and annotate random data points 
+set.seed(1234)
+par(mar = c(0,0,0,0))
+x <- rnorm(12, mean = rep(1:3, each = 4), sd = 0.2)
+y <- rnorm(12, mean = rep(c(1,2,1), each = 4), sd = 0.2)
+plot(x,y, col="blue", pch=19,cex=2)
+text(x+0.05,y+0.05, labels = as.character(1:12))
+dataFrame <- data.frame(x=x, y=y)
+
+kmeansObj <- kmeans(dataFrame, centers = 3)
+names(kmeansObj)
+#clusters has the clustering of the points
+plot(x,y,col = kmeansObj$cluster, pch = 19, cex = 2)
+points(kmeansObj$centers, col = 1:3, pch = 3, cex = 3, lwd = 3)
+
+#we can also see clustering from k-means in a heatmap-style
+#using image() function
+set.seed(144)
+dataMatrix <- as.matrix(dataFrame)[sample(1:12),]
+kmeansObj2 <- kmeans(dataMatrix, centers = 3)
+par(mfrow = c(1,2), mar = c(2,4,0.1,0.1))
+image(t(dataMatrix)[,nrow(dataMatrix):1]) #ordered as in the matrix
+image(t(dataMatrix)[,order(kmeansObj2$cluster)]) #ordered witihn clusters
+
+
+##DIMENSION REDUCTION
+#exploratory pattern analysis
+set.seed(144)
+par(mar = rep(4,4))
+dataMatrix <- matrix(rnorm(400), nrow = 40)
+image(1:10,1:40, t(dataMatrix))[,nrow(dataMatrix):1] 
+heatmap(dataMatrix) #shows heatmap
+
+#reorder rows according to clusters
+hh <- hclust(dist(dataMatrix))
+dataMatrixOrdered <- dataMatrix[hh$order, ]
+par(mfrow = c(1,3))
+image(1:10,1:40, t(dataMatrixOrdered))[,nrow(dataMatrixOrdered):1] 
+plot(rowMeans(dataMatrixOrdered), 40:1,xlab = "Row Mean", ylab = "Row", pch = 19)
+plot(colMeans(dataMatrixOrdered), xlab = "Column", ylab = "Column mean", pch = 19)
+#we don't see any pattern as the numbers were uniformly randomly generated
+
+#PRINCIPAL COMPONENT ANALAYSIS // SINGULAR VARIABLE DECOMPOSITION
+#find a new set of fewer uncorrelated variables that explains
+#as much variance as possible of the original data
+#SVD is finding a decomposition of matrix X as: X = U*D*V(transpose)
+#where U is called left singular vectors
+#V is called right singular vectors
+#D is singular values
+#PCA is the same as SVD, but before decomposing the mean is substracted from
+#each column and then each one is divided by the standar deviation
+
+
 
